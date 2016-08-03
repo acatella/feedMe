4339ce30af3215b0f93ef49480e3e5f10cee8dca
acatella <aficclark@gmail.com>
2016-08-02 22:13:35 -0700

    added factory and resource call to scaphold api

---
+++ app/scripts/services/getposts.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name feedmeApp.getPosts
+ * @description
+ * # getPosts
+ * Factory in the feedmeApp.
+ */
+angular.module('feedmeApp')
+  .factory('getPosts', function () {
+
+    return {
+
+      var data = {
+          query: 'query { viewer{ allPosts { edges { node { id createdAt modifiedAt title content } } }  } } ',
+          variables: ""
+      };
+
+      getPosts: function () {
+        return $http.post("https://api.scaphold.io/graphql/76f8d00e-08f8-4590-ad92-5eba957cc42e", data, function(result) {
+            console.log("That was easy!");
+            console.log(result);
+        })
+
+      }
+    };
+  });
